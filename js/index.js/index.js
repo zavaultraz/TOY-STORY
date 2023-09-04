0f3b5377188ae1fc838474fd7e1af2eb2b0b5596
zavaultraz <zavazaneta@gmail.com>
2023-09-04 13:51:45 +0700

    first project

--- js/index.js/index.js
+++ js/index.js/index.js
@@ -4,6 +4,5 @@ const navbarItem = document.getElementsByClassName('navbar-item');
 toggleButton.addEventListener('click', function() {
     for(let i = 0; i < navbarItem.length; i++) {
         navbarItem[i].classList.toggle('active');
-
     }
 });
